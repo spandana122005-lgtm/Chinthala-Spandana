b7566159836c9792eee402f173deb44d5e3c223d
Chinthala Spandana <spandana122005@gmail.com>
2026-06-29 20:58:19 +0530

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,8 @@
+a=int(input("Enter first number:"))
+
+b=int(input("Enter the second number:))
+            sum= a+b
+            sub= a-b
+            print("Sum=",sum)
+            print("sub=",sub)
+            
